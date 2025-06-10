bfb850fc2dc3637ecde70018e08a529bfbb53829
kushal <kushal@gmail.com>
2025-06-10 14:45:42 +0000

    parameter pipeline

--- Jenkinsfile-param.groovy
+++ Jenkinsfile-param.groovy
@@ -1,5 +1,6 @@
 pipeline {
     agent any
+     parameters { choice(name: 'DOCKER_REPO', choices: ['dockerhub', 'awsecr'], description: 'Select the docker image repo') }
 
     stages {
        
@@ -9,26 +10,45 @@ pipeline {
             }
         }
         stage('docker login ') {
+            when {
+                    expression { params.DOCKER_REPO == 'dockerhub' }
+                }
+
             steps {
                 sh 'docker login '
             }
         }
         stage('docker tagging ') {
+           when {
+                    expression { params.DOCKER_REPO == 'dockerhub' }
+                }
+
             steps {
                 sh 'docker tag java-spring-22:v${BUILD_NUMBER} malleshdevops/dev22:spring-22.${BUILD_NUMBER}'
             }
         }
       stage('image push dockerhub ') {
+           when {
+                    expression { params.DOCKER_REPO == 'dockerhub' }
+                }
+
             steps {
-                sh 'docker push malleshdevops/dev22:spring-22.${BUILD_NUMBER}'
+                sh 'docker push malleshdevops/dev22:spring-22.{BUILD_NUMBER}'
             }
         }
       stage('authenticate ECR'){
+         when {
+                    expression { params.DOCKER_REPO == 'awsecr' }
+                }
+
            steps{
            sh 'aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 759449706669.dkr.ecr.us-west-2.amazonaws.com'
           }
       }
       stage('push ECR'){
+           when {
+                    expression { params.DOCKER_REPO == 'awsecr' }
+                }
             steps {
                 sh '''
 		 docker tag java-spring-22:v{BUILD_NUMBER} 759449706669.dkr.ecr.us-west-2.amazonaws.com/dev22/spring:v${BUILD_NUMBER}
